58ce093b2c02a01608cc3f2a4da627bc66a3584b
Kushagra_Shukla <73269482+KushagraShukla004@users.noreply.github.com>
2025-02-28 14:02:12 +0530

    simple UI stuff

--- client/src/pages/BuyCredit.jsx
+++ client/src/pages/BuyCredit.jsx
@@ -2,10 +2,11 @@ import PricingSection from "../components/PricingSection";
 
 const BuyCredit = () => {
   return (
-    <div className="flex items-center justify-center min-h-screen mt-[-50px]">
+    <div className="flex items-center justify-center min-h-screen mt-[-100px] flex-col">
       {/* using BuyCredit page props to call onClick even in PricingSection Component */}
       {/* or */}
-
+      {/* use AppContext in the component itself */}
+      <h1 className="text-4xl sm:text-6xl font-bold text-gray-900">Buy Credits</h1>
       <PricingSection />
     </div>
   );
